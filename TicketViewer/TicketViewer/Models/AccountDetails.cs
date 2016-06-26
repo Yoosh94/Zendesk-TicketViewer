a28b94345d2eceebc56d3503714a6c066a5b328f
Ayush Kumar <ayush_94@hotmail.com>
2016-06-26 21:11:30 +1000

    Create account detail class

---
+++ TicketViewer/TicketViewer/Models/AccountDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketViewer.Models
+{
+    public static class AccountDetails
+    {
+        public static string requestUri = @"https://firstpointhelp.zendesk.com/";
+        
+    }
+}
